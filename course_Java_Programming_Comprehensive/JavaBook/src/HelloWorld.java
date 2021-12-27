6ca2a68a434416d0b396b48307a6165ea1f246d2
michedomingo <michedomingo@gmail.com>
2021-12-26 18:08:38 -0800

    Update HelloWorld.java comments

--- course_Java_Programming_Comprehensive/JavaBook/src/HelloWorld.java
+++ course_Java_Programming_Comprehensive/JavaBook/src/HelloWorld.java
@@ -1,17 +1,17 @@
-import javax.swing.*; 
+import javax.swing.*;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-	// create a window with the title "My First Program"
-    JFrame frame = new JFrame("My First Program");
-    
-    // add "blimp.png" picture in file to this window
-    frame.getContentPane().add(new JLabel(new ImageIcon("blimp.png")));
-    
-    // set the window size so that the contents fit
-    frame.pack();
-    
-    // make the window visible on computer screen
-    frame.setVisible(true);				
-  }
+    public static void main(String[] args) {
+        // create a window with the title "My First Program"
+        JFrame frame = new JFrame("My First Program");
+
+        // add "blimp.png" picture in file to this window
+        frame.getContentPane().add(new JLabel(new ImageIcon("blimp.png")));
+
+        // set the window size so that the contents fit
+        frame.pack();
+
+        // make the window visible on computer screen
+        frame.setVisible(true);
+    }
 }
