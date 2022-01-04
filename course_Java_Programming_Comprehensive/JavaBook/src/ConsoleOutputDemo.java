9e214ad2abcb058a46b9fdbed4132376118f6f6c
michedomingo <michedomingo@gmail.com>
2022-01-04 08:55:05 -0800

    Add ConsoleOutputDemo.java

---
+++ course_Java_Programming_Comprehensive/JavaBook/src/ConsoleOutputDemo.java
@@ -0,0 +1,6 @@
+public class ConsoleOutputDemo {
+    public static void main(String[] args) {
+        System.out.println("Text output is useful for debugging programs.");
+        System.out.println("Graphics is more fun, though.");
+    }
+}
