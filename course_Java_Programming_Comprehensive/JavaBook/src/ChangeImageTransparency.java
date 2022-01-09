621a2f6f12ec71f762b606b98d53bb69b6157422
michedomingo <michedomingo@gmail.com>
2022-01-09 07:12:05 -0800

    Add ChangeImageTransparency.java(p70)

---
+++ course_Java_Programming_Comprehensive/JavaBook/src/ChangeImageTransparency.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+
+public class ChangeImageTransparency {
+    public static void main(String[] args) {
+        System.out.println("White: " + Color.WHITE);
+        System.out.println("Black: " + Color.BLACK);
+        System.out.println("Red: " + Color.RED);
+        System.out.println("Green: " + Color.GREEN);
+        System.out.println("Blue: " + Color.BLUE);
+        System.out.println("Yellow: " + Color.YELLOW);
+        System.out.println("Orange: " + Color.ORANGE);
+        System.out.println("Pink: " + Color.PINK);
+    }
+}
+
+// prints values of components for several colors
+// ch3_p70_pdf75
