490eea83ff85bc3e6da0db65036c7e3f1adc55e1
michedomingo <michedomingo@gmail.com>
2022-01-10 13:13:37 -0800

    Add ChangeImageColors.java (p124)

---
+++ course_Java_Programming_Comprehensive/JavaBook/src/ChangeImageColors.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import com.programwithjava.basic.DrawingKit;
+
+public class ChangeImageColors {
+    public static void main(String[] args) {
+        // create a window on computer screen using DrawingKit constructor
+        // specify window size (update from default WxH: 500x500)
+        DrawingKit dk = new DrawingKit("Change colors", 1680, 700);
+        BufferedImage picture = dk.loadPicture("image/daffodils.jpg");
+        // draw the original picture
+        dk.drawPicture(picture, 0, 50);
+
+        // modify the pixels in picture
+        for (int x = 0; x < picture.getWidth(); x++) {
+            for (int y = 0; y < picture.getHeight(); y++) {
+                // get red, green, & blue components of pixel at (x, y)
+                int colorValue = picture.getRGB(x, y);
+                Color pixelColor = new Color(colorValue);
+                int red = pixelColor.getRed();
+                int green = pixelColor.getGreen();
+                int blue = pixelColor.getBlue();
+
+                if (red > 245 && green > 245 && blue > 245) {
+                    // change a white or nearly white pixel to blue
+                    red = 100;
+                    green = 150;
+                    blue = 255;
+                } else if (red >= 2 * green) {
+                    // leave an orange pixel unchanged
+                    ;
+                } else if (red >= green) {
+                    // change a yellow pixel to pale pink
+                    blue = green;
+                }
+
+                // update the pixel color in picture
+                Color newPixelColor = new Color(red, green, blue);
+                int newRgbvalue = newPixelColor.getRGB();
+                picture.setRGB(x, y, newRgbvalue);
+            } // end inner for loop
+        } // end outer for loop
+
+        // draw modified picture
+        dk.drawPicture(picture, 800, 50);
+    }
+}
+
+// mainipulate selectively the colors in photo
+// change color of white background and yellow petals
+// ch4_p119_pdf124
