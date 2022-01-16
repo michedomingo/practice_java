d72e0c0b17d3f7802dd8aea2ec34577952e79a19
michedomingo <michedomingo@gmail.com>
2022-01-16 13:09:10 -0800

    Add hw1, ch5, ex21 (p195)

---
+++ course_Java_Programming_Comprehensive/hw1/src/ch5hw/ex21/Compositor.java
@@ -0,0 +1,63 @@
+package ch5hw.ex21;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import com.programwithjava.basic.DrawingKit;
+
+public class Compositor {
+    private static final int MAX_VALUE = 255;
+
+    // this method returns a new image (created with image1 & image2)
+    public BufferedImage weightedAdd(BufferedImage image1, BufferedImage image2, double weight) {
+        int width = Math.min(image1.getWidth(), image2.getWidth());
+        int height = Math.min(image1.getHeight(), image2.getHeight());
+        double weight2 = 1 - weight;
+
+        BufferedImage image3 = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                // get samples of pixel at (x, y) in image 1
+                int colorValue1 = image1.getRGB(x, y);
+                Color pixelColor1 = new Color(colorValue1);
+                int red1 = pixelColor1.getRed();
+                int green1 = pixelColor1.getGreen();
+                int blue1 = pixelColor1.getBlue();
+
+                // get samples of pixel at (x, y) in image 2
+                int colorValue2 = image2.getRGB(x, y);
+                Color pixelColor2 = new Color(colorValue2);
+                int red2 = pixelColor2.getRed();
+                int green2 = pixelColor2.getGreen();
+                int blue2 = pixelColor2.getBlue();
+
+                // add samples using different weights to create a new color
+                int red3 = Math.min((int) (weight * red1) + (int) (weight2 * red2), MAX_VALUE);
+                int green3 = Math.min((int) (weight * green1) + (int) (weight2 * green2), MAX_VALUE);
+                int blue3 = Math.min((int) (weight * blue1) + (int) (weight2 * blue2), MAX_VALUE);
+
+                // set color of a pixel in image3
+                Color newPixelColor = new Color(red3, green3, blue3);
+                int newRgbvalue = newPixelColor.getRGB();
+                image3.setRGB(x, y, newRgbvalue);
+            }
+        }
+        return image3;
+    }
+
+    public static void main(String[] args) {
+        DrawingKit dk = new DrawingKit("Compositor", 1250, 1450);
+        BufferedImage p1 = dk.loadPicture("image/pattern1.jpg");
+        BufferedImage p2 = dk.loadPicture("image/pattern2.jpg");
+        Compositor c = new Compositor();
+
+        BufferedImage p3 = c.weightedAdd(p1, p2, 0.20);
+        dk.drawPicture(p3, 0, 25);
+
+        BufferedImage p4 = c.weightedAdd(p1, p2, 0.35);
+        dk.drawPicture(p4, 0, 700);
+    }
+}
+
+// manipulates & combines 2 images to produce a new image
+// ch5_p148_pdf153, hw1_p190_pdf195_ex21
