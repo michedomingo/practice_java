3fd6c5e060656b44563882de378ecbfd30011fd7
michedomingo <michedomingo@gmail.com>
2022-01-24 14:40:43 -0800

    Update wip hw2, ch6, ex7 (p262)

--- course_Java_Programming_Comprehensive/hw2/src/ch6ex7/Account.java
+++ course_Java_Programming_Comprehensive/hw2/src/ch6ex7/Account.java
@@ -10,7 +10,7 @@ public class Account {
 
     // constructs new Account with initialized values
     public Account() {
-        this(1234567, "myAccount", new BigDecimal("1000"), new BigDecimal("0.5"));
+        this(1234567, "myAccount1", new BigDecimal("1000"), new BigDecimal("0.5"));
     }
 
     // constructs new Account with the specified arguments
@@ -23,41 +23,40 @@ public class Account {
 
     // method to display Account number
     public int getNumber() {
-        // System.out.println(String.format("number of Account: %d", number));
         return number;
     }
 
     // method to display Account name
     public String getName() {
-        // System.out.println(String.format("name of Account: %s", name));
         return name;
     }
 
     // method to display Account balance
     public BigDecimal getBalance() {
         balance = balance.setScale(2, RoundingMode.HALF_UP);
-        // System.out.println("balance on Account: " + balance.toString());
         return balance;
     }
 
     // method to display Account interest rate
     public BigDecimal getInterestRate() {
         interestRate = interestRate.setScale(2, RoundingMode.HALF_UP);
-        // System.out.println("interestRate on Account: " + interestRate.toString());
         return interestRate;
     }
 
     public static void main(String[] args) {
-        Account testAccount = new Account();
-        System.out.println(testAccount.getNumber());
-        System.out.println(testAccount.getName());
-        System.out.println(testAccount.getBalance());
-        System.out.println(testAccount.getInterestRate());
-        Account testAccount2 = new Account(987654321, "myAccount2", new BigDecimal("500"), new BigDecimal("0.25"));
-        System.out.println(testAccount2.getNumber());
-        System.out.println(testAccount2.getName());
-        System.out.println(testAccount2.getBalance());
-        System.out.println(testAccount2.getInterestRate());
+        Account testAccount1 = new Account();
+        System.out.println("------- testAccount1 -------");
+        System.out.println(String.format("number of Account: %d", testAccount1.getNumber()));
+        System.out.println(String.format("name of Account: %s", testAccount1.getName()));
+        System.out.println("balance on Account: " + testAccount1.getBalance());
+        System.out.println("interestRate on Account: " + testAccount1.getInterestRate());
+        System.out.println();
+        System.out.println("------- testAccount2 -------");
+        Account testAccount2 = new Account(9876543, "myAccount2", new BigDecimal("500"), new BigDecimal("0.25"));
+        System.out.println(String.format("number of Account: %d", testAccount2.getNumber()));
+        System.out.println(String.format("name of Account: %s", testAccount2.getName()));
+        System.out.println("balance on Account: " + testAccount2.getBalance());
+        System.out.println("interestRate on Account: " + testAccount2.getInterestRate());
     }
 }
 
--- course_Java_Programming_Comprehensive/hw2/src/ch6ex7/SavingsAccount.java
+++ course_Java_Programming_Comprehensive/hw2/src/ch6ex7/SavingsAccount.java
@@ -16,14 +16,16 @@ public class SavingsAccount extends Account {
 
     // constructor 2
     public SavingsAccount(int dd, int mm, int yyyy) {
+        super(7654321, "saveAccount2", new BigDecimal("1000"), new BigDecimal("0.5"));
         day = dd;
         month = mm;
         year = yyyy;
+        savingsBalance = getBalance();
     }
 
     public void deposit(BigDecimal amount) {
         // BigDecimal savingsBalance = getBalance();
-        System.out.println("(inside deposit method) balance inherited from class Account: " + savingsBalance);
+        System.out.println("(inside deposit method) current balance: " + savingsBalance);
         System.out.println("deposit on SavingsAccount: " + amount);
 
         savingsBalance = savingsBalance.add(amount);
@@ -31,7 +33,7 @@ public class SavingsAccount extends Account {
     }
 
     public boolean withdraw(BigDecimal amount) {
-        System.out.println("(inside withdraw method) balance inherited from class Account: " + savingsBalance);
+        System.out.println("(inside withdraw method) current balance: " + savingsBalance);
         System.out.println("withdraw on SavingsAccount: " + amount);
 
         savingsBalance = savingsBalance.subtract(amount);
@@ -48,20 +50,29 @@ public class SavingsAccount extends Account {
     // determine interest on a given balance at end of a year
     public static void main(String[] args) {
         SavingsAccount saveAccount1 = new SavingsAccount();
-
+        System.out.println("------- saveAccount1 -------");
         System.out.println(
-                String.format("day: %d, month: %d, year: %d", saveAccount1.day, saveAccount1.month, saveAccount1.year));
+                String.format("day: %d, month: %d, year: %d", saveAccount1.day,
+                        saveAccount1.month, saveAccount1.year));
         System.out.println();
-
         saveAccount1.deposit(new BigDecimal("5000"));
-
         System.out.println();
         saveAccount1.withdraw(new BigDecimal("250"));
+
+        System.out.println();
+
+        SavingsAccount saveAccount2 = new SavingsAccount(02, 02, 2022);
+        System.out.println("------- saveAccount2 -------");
+        System.out.println(
+                String.format("day: %d, month: %d, year: %d", saveAccount2.day, saveAccount2.month, saveAccount2.year));
+        System.out.println();
+        saveAccount2.deposit(new BigDecimal("5000"));
+        System.out.println();
+        saveAccount2.withdraw(new BigDecimal("250"));
 
         // saveAccount1.computeInterest();
         // System.out.println("balance inherited from class Account: " +
         // saveAccount1.getBalance());
-
         // System.out.println(saveAccount1.getNumber());
         // System.out.println(saveAccount1.getName());
         // System.out.println(saveAccount1.getBalance());
