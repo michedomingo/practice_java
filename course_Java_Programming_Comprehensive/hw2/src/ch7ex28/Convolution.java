a297f4b6cce1eca164e131a66563d2deda65f9e6
michedomingo <michedomingo@gmail.com>
2022-02-11 18:24:37 -0800

    Update wip hw2, ch7, ex28 (p331)

--- course_Java_Programming_Comprehensive/hw2/src/ch7ex28/Convolution.java
+++ course_Java_Programming_Comprehensive/hw2/src/ch7ex28/Convolution.java
@@ -12,6 +12,21 @@ public class Convolution {
     // // some data structure (maybe 3D array?) to hold filters / kernals
     // }
 
+    public int[][][] applyFilter(int[][][] imgArr3D, int imgRows, int imgColumns, int[][] kernal) {
+        int[][][] modifiedImgArr3D = new int[3][imgRows][imgColumns];
+
+        int[][][] copiedImgArr3D = new int[3][imgRows][imgColumns];
+        for (int row = 0; row < imgRows; row++) { // make working copy to avoid original image data changes
+            for (int col = 0; col < imgColumns; col++) {
+                copiedImgArr3D[0][row][col] = imgArr3D[0][row][col];
+                copiedImgArr3D[1][row][col] = imgArr3D[1][row][col];
+                copiedImgArr3D[2][row][col] = imgArr3D[2][row][col];
+            }
+        }
+
+        return modifiedImgArr3D;
+    }
+
 }
 
 // hw2_ch7_p331_pdf336_ex28
--- course_Java_Programming_Comprehensive/hw2/src/ch7ex28/FilteredImage.java
+++ course_Java_Programming_Comprehensive/hw2/src/ch7ex28/FilteredImage.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 import com.programwithjava.basic.DrawingKit;
 
 // MAIN DRIVER
-public class FilteredImage {
+public class FilteredImage extends Convolution {
 
     public static final int[][] SHARPENING_KERNAL = { { -1, -1, -1 }, { -1, 9, -1 }, { -1, -1, -1 } };
     public static final int[][] BLURRING_KERNAL = { { -1, -1, -1 }, { -1, 9, -1 }, { -1, -1, -1 } };
@@ -19,31 +19,25 @@ public class FilteredImage {
     // public int kernalRows; // rows = height
     // public int kernalColumns; // columns = width
     public int[][][] imgArr3D; // stores originalImage pixel data
-    // public int[][][] imgArrMod3D; // stores resultingImage pixel data
+    public int[][][] imgArrMod3D; // stores resultingImage pixel data
     // public int[] imgArr1D; // stores pixel data...
     // Convolution convolutionObject; // reference to image-processing object ?
 
-    // method makes a 2D array representing the inputImage
-    public int[][][] makeimgArr3D(BufferedImage imageToArray) {
-        imgArr3D = new int[3][imgRows][imgColumns];
+    // method populates array w/ inputImage pixel data
+    public int[][][] makeImgArr3D(BufferedImage pixelDataToArray) {
+        int[][][] pixelData = new int[3][imgRows][imgColumns];
 
-        for (int row = 0; row < imgRows; row++) { // getHeight()
-            for (int col = 0; col < imgColumns; col++) { // getWidth
-                // get red, green, & blue components of pixel at (row, col)
-                Color pixelColor = new Color(imageToArray.getRGB(col, row));
-                imgArr3D[0][row][col] = pixelColor.getRed();
-                imgArr3D[1][row][col] = pixelColor.getGreen();
-                imgArr3D[2][row][col] = pixelColor.getBlue();
+        for (int row = 0; row < imgRows; row++) { // .getHeight()
+            for (int col = 0; col < imgColumns; col++) { // .getWidth()
+                // get red, green, & blue components of pixel at (col, row)
+                Color pixelColor = new Color(pixelDataToArray.getRGB(col, row));
+                pixelData[0][row][col] = pixelColor.getRed();
+                pixelData[1][row][col] = pixelColor.getGreen();
+                pixelData[2][row][col] = pixelColor.getBlue();
 
             }
         }
-        return imgArr3D;
-    }
-
-    public int[][] applyConvolutionOperator(int kernal) {
-        int[][] newRed = doSomeConvolution(imgArr3D[0], imgRows, imgColumns, kernal, 3, 3, 1);
-        int[][] newGreen = doSomeConvolution(imgArr3D[1], imgRows, imgColumns, kernal, 3, 3, 1);
-        int[][] newBlue = doSomeConvolution(imgArr3D[2], imgRows, imgColumns, kernal, 3, 3, 1);
+        return pixelData;
     }
 
     // method to process inputImage by
@@ -54,11 +48,12 @@ public class FilteredImage {
         imgRows = inputImage.getHeight();
         imgColumns = inputImage.getWidth();
 
-        imgArr3D = makeimgArr3D(inputImage);
+        imgArr3D = makeImgArr3D(inputImage);
+        imgArrMod3D = applyFilter(imgArr3D, imgRows, imgColumns, kernal);
 
         // BufferedImage resultingImage = new BufferedImage(imgRows, imgColumns,
         // BufferedImage.TYPE_INT_ARGB);
-        resultingImage = inputImage; // placeholder process pending update
+        resultingImage = inputImage; // placeholder pending method...
 
         return resultingImage;
     }
