56ba129d6cbe307d4961354d04ca074781bbbe1a
michedomingo <michedomingo@gmail.com>
2022-02-17 11:18:57 -0800

    Update wip hw3, ch9, ex21 (p461)

--- course_Java_Programming_Comprehensive/hw4/src/ch9ex21/ModifiedPhoto.java
+++ course_Java_Programming_Comprehensive/hw4/src/ch9ex21/ModifiedPhoto.java
@@ -3,11 +3,14 @@ package ch9ex21;
 import java.awt.*;
 // import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
 
 public class ModifiedPhoto {
     private JFrame window;
     private JPanel topPanel, groupPanel;
     private ModifiedPhotoDrawingPanel drawingPanel;
+    private JPanel titlePanel;
+    private JLabel titleLabel;
     // private ButtonGroup group;
 
     public ModifiedPhoto() {
@@ -15,6 +18,9 @@ public class ModifiedPhoto {
         // group = new ButtonGroup();
 
         createPanels();
+        titlePanel = createTitlePanel(); // panel to hold photo's title
+        titleLabel = new JLabel("birds.jpeg"); // create label
+        titlePanel.add(titleLabel);
 
         window.setContentPane(topPanel);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,6 +36,21 @@ public class ModifiedPhoto {
         // add drawingPanel to topPanel
         topPanel.add(drawingPanel, BorderLayout.CENTER);
         topPanel.add(groupPanel, BorderLayout.EAST);
+
+        // add borders and backgrounds
+        groupPanel.setLayout(new BoxLayout(groupPanel, BoxLayout.Y_AXIS)); // add components to a single column
+        groupPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        groupPanel.setBackground(Color.lightGray);
+        topPanel.setBackground(Color.lightGray);
+    }
+
+    // method creates light gray panel at top of window to label photo
+    public JPanel createTitlePanel() {
+        JPanel titlePanel = new JPanel();
+        topPanel.add(titlePanel, BorderLayout.NORTH);
+        titlePanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        titlePanel.setBackground(Color.lightGray);
+        return titlePanel;
     }
 
     public static void main(String[] args) {
