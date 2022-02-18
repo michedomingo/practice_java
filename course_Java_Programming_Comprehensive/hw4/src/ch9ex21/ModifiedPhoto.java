de5f5df5702e53bb1befecdce2a45b9493dbdfd8
michedomingo <michedomingo@gmail.com>
2022-02-17 16:20:45 -0800

    Update wip hw3, ch9, ex21 (p461)

--- course_Java_Programming_Comprehensive/hw4/src/ch9ex21/ModifiedPhoto.java
+++ course_Java_Programming_Comprehensive/hw4/src/ch9ex21/ModifiedPhoto.java
@@ -1,9 +1,9 @@
 package ch9ex21;
 
 import java.awt.*;
-// import java.awt.event.*;
+import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.border.EtchedBorder;
+import javax.swing.border.*;
 
 public class ModifiedPhoto {
     private JFrame window;
@@ -11,16 +11,16 @@ public class ModifiedPhoto {
     private ModifiedPhotoDrawingPanel drawingPanel;
     private JPanel titlePanel;
     private JLabel titleLabel;
-    // private ButtonGroup group;
 
     public ModifiedPhoto() {
         window = new JFrame("Modified Photo");
-        // group = new ButtonGroup();
 
         createPanels();
         titlePanel = createTitlePanel(); // panel to hold photo's title
         titleLabel = new JLabel("birds.jpeg"); // create label
         titlePanel.add(titleLabel);
+
+        groupPanel.add(createShiftPanel()); // create shift panel
 
         window.setContentPane(topPanel);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,24 +33,66 @@ public class ModifiedPhoto {
         drawingPanel = new ModifiedPhotoDrawingPanel();
         groupPanel = new JPanel();
 
-        // add drawingPanel to topPanel
+        // add drawingPanel & groupPanel to topPanel
         topPanel.add(drawingPanel, BorderLayout.CENTER);
-        topPanel.add(groupPanel, BorderLayout.EAST);
+        topPanel.add(groupPanel, BorderLayout.SOUTH);
 
-        // add borders and backgrounds
-        groupPanel.setLayout(new BoxLayout(groupPanel, BoxLayout.Y_AXIS)); // add components to a single column
+        // add borders & backgrounds
+        groupPanel.setLayout(new BoxLayout(groupPanel, BoxLayout.X_AXIS)); // add components to a single row
         groupPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-        groupPanel.setBackground(Color.lightGray);
-        topPanel.setBackground(Color.lightGray);
+        groupPanel.setBackground(Color.red);
+        topPanel.setBackground(Color.blue);
+        topPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED),
+                BorderFactory.createEtchedBorder(EtchedBorder.LOWERED)));
     }
 
-    // method creates light gray panel at top of window to label photo
+    // creates light gray panel at top of window to label photo
     public JPanel createTitlePanel() {
         JPanel titlePanel = new JPanel();
         topPanel.add(titlePanel, BorderLayout.NORTH);
         titlePanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
         titlePanel.setBackground(Color.lightGray);
         return titlePanel;
+    }
+
+    // creates panel with buttons to shift the image when clicked
+    public JPanel createShiftPanel() {
+        JPanel buttonPanel = new JPanel();
+
+        JButton shiftNorthButton = new JButton("North");
+        JButton shiftSouthButton = new JButton("South");
+        JButton shiftEastButton = new JButton("East");
+        JButton shiftWestButton = new JButton("West");
+        buttonPanel.add(shiftNorthButton);
+        buttonPanel.add(shiftSouthButton);
+        buttonPanel.add(shiftEastButton);
+        buttonPanel.add(shiftWestButton);
+
+        shiftNorthButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                drawingPanel.shiftImage(0, -20); // translate image north
+            }
+        });
+
+        shiftSouthButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                drawingPanel.shiftImage(0, 20); // translate image south
+            }
+        });
+
+        shiftEastButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                drawingPanel.shiftImage(20, 0); // translate image east
+            }
+        });
+
+        shiftWestButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                drawingPanel.shiftImage(-20, 0); // translate image west
+            }
+        });
+
+        return buttonPanel;
     }
 
     public static void main(String[] args) {
--- course_Java_Programming_Comprehensive/hw4/src/ch9ex21/ModifiedPhotoDrawingPanel.java
+++ course_Java_Programming_Comprehensive/hw4/src/ch9ex21/ModifiedPhotoDrawingPanel.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 public class ModifiedPhotoDrawingPanel extends JPanel {
     private Image backgroundImage;
 
+    private int shiftX = 0; // shift image along x-axis
+    private int shiftY = 0; // shift image along y-axis
+
     public ModifiedPhotoDrawingPanel() {
         backgroundImage = new javax.swing.ImageIcon("image/birds.jpeg").getImage();
     }
@@ -14,7 +17,17 @@ public class ModifiedPhotoDrawingPanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+
+        // shift image
+        g2.translate(shiftX, shiftY);
+
         g2.drawImage(backgroundImage, 0, 0, null);
+    }
+
+    public void shiftImage(int x, int y) {
+        shiftX += x;
+        shiftY += y;
+        repaint();
     }
 
 }
